9a0beed346a81af438b1ac0048bd33ea957207d4
Bent Bracke <bent@bracke.dk>
2021-03-10 17:58:11 +0100

    Removed unnecessary . from description

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,7 @@ module.exports = function (environment) {
 
     blog: {
       title: 'Bracke',
-      description: 'The personal blog of Bent Bracke.',
+      description: 'The personal blog of Bent Bracke',
       coverImage: '/images/blog-cover.jpg',
 
       navigation: [
